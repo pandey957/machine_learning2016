f6acd92438650be16739a1b076ae788f5dd7e655
Satyendra Raj Pandey <pandey957@gmail.com>
2016-06-23 11:33:47 +0530

    completed pca and clustering

--- machine-learning-ex7/ex7/pca.m
+++ machine-learning-ex7/ex7/pca.m
@@ -19,13 +19,7 @@ S = zeros(n);
 % Note: When computing the covariance matrix, remember to divide by m (the
 %       number of examples).
 %
-
-
-
-
-
-
-
+[U,S,V] = svd((X' * X) /m);
 % =========================================================================
 
 end
--- machine-learning-ex7/ex7/recoverData.m
+++ machine-learning-ex7/ex7/recoverData.m
@@ -8,7 +8,6 @@ function X_rec = recoverData(Z, U, K)
 
 % You need to return the following variables correctly.
 X_rec = zeros(size(Z, 1), size(U, 1));
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the approximation of the data by projecting back
 %               onto the original space using the top K eigenvectors in U.
@@ -20,9 +19,7 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %
 %               Notice that U(j, 1:K) is a row vector.
 %               
-
-
-
+X_rec = Z * U(:,1:K)';
 % =============================================================
 
 end
