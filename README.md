56a728f894536476435233f83760279805f14a50
Satyendra Raj Pandey <pandey957@gmail.com>
2016-06-29 12:45:24 +0530

    completed cf cost

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # machine_learning2016
-<p> This repository contains the completed assignment for Machine Learning Courser By Andrew Ng.
+<p> This repository contains the completed assignment for Machine Learning Course By Andrew Ng.
--- machine-learning-ex8/ex8/ex8_cofi.m
+++ machine-learning-ex8/ex8/ex8_cofi.m
@@ -62,6 +62,7 @@ R = R(1:num_movies, 1:num_users);
 J = cofiCostFunc([X(:) ; Theta(:)], Y, R, num_users, num_movies, ...
                num_features, 0);
            
+           
 fprintf(['Cost at loaded parameters: %f '...
          '\n(this value should be about 22.22)\n'], J);
 
